8f6df85a9acafb646602c48d34e066e5a8bf8a34
Claudemir Ramos Ferreira <claudemirramosferreira@gmail.com>
2017-12-06 16:56:28 -0400

    IOncluir profile heroku

--- pdv-rest/src/main/java/br/com/pdv/controller/ProdutoController.java
+++ pdv-rest/src/main/java/br/com/pdv/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,30 +22,36 @@ import br.com.pdv.service.ProdutoService;
 @RequestMapping("produtos/")
 public class ProdutoController {
 
-	@Autowired
-	private ProdutoService service;
+    @Autowired
+    private ProdutoService service;
 
-	@GetMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<ProdutoDTO>> listar() {
-		List<ProdutoDTO> lista = service.findAll();
-		return new ResponseEntity<>(lista, HttpStatus.OK);
-	}
+    @GetMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ProdutoDTO>> listar() {
+        List<ProdutoDTO> lista = service.findAll();
+        return new ResponseEntity<>(lista, HttpStatus.OK);
+    }
 
-	@GetMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ProdutoDTO> findId(@PathVariable(value = "id") String id) {
-		ProdutoDTO dto = service.findId(new Long(id));
-		return new ResponseEntity<>(dto, HttpStatus.OK);
-	}
+    @GetMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProdutoDTO> findId(@PathVariable(value = "id") String id) {
+        ProdutoDTO dto = service.findId(new Long(id));
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
 
-	@PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ProdutoDTO> save(@RequestBody ProdutoDTO dto) {
-		dto = service.save(dto);
-		return new ResponseEntity<>(dto, HttpStatus.OK);
-	}
+    @DeleteMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> delete(@PathVariable(value = "id") String id) {
+        service.delete(new Long(id));
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
 
-	@PutMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ProdutoDTO> upfate(@RequestBody ProdutoDTO dto) {
-		dto = service.update(dto);
-		return new ResponseEntity<>(dto, HttpStatus.OK);
-	}
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProdutoDTO> save(@RequestBody ProdutoDTO dto) {
+        dto = service.save(dto);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProdutoDTO> upfate(@RequestBody ProdutoDTO dto) {
+        dto = service.update(dto);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
 }
--- pdv-rest/src/test/java/br/com/pdv/PdvRestApplicationTests.java
+++ pdv-rest/src/test/java/br/com/pdv/PdvRestApplicationTests.java
@@ -4,25 +4,27 @@ import java.math.BigDecimal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.pdv.enumerated.UnidadeMedidaEnum;
+import br.com.pdv.model.dao.ProdutoDAO;
 import br.com.pdv.model.entity.Produto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PdvRestApplicationTests {
 
-	// @Autowired
-	// ProdutoRepository repository;
+	@Autowired
+	ProdutoDAO repository;
 
 	@Test
 	public void contextLoads() {
 		Produto p = new Produto("claudemir", "123455", new BigDecimal(22), new BigDecimal(22), (long) 0,
 				UnidadeMedidaEnum.UNIDADE);
 		// p.setId((Long) null);
-		// repository.create(p);
+		repository.save(p);
 	}
 
 }
