9c1c9fc65e4b85734d7804554d969ac794cc7ee5
Nelson Sozinho <nelsonsozinho@gmail.com>
2018-01-04 23:53:00 -0400

    validacao basica do formulario de authenticacao

--- pdv-front/src/app/login/login.component.ts
+++ pdv-front/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../router.animations';
 import {Router} from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import {LoginService} from './login.service';
 import { Auth } from '../shared/model/auth.model';
@@ -9,7 +9,6 @@ import { Input } from '@angular/core/src/metadata/directives';
 import { Route } from '@angular/compiler/src/core';
 import { Routes } from '@angular/router/src/config';
 import { DashboardComponent } from '../layout/dashboard/dashboard.component';
-
 
 @Component({
     selector: 'app-login',
@@ -21,21 +20,40 @@ export class LoginComponent implements OnInit {
 
     auth: Auth;
     message: string;
+    form: FormGroup;
+
+    private formSumitAttempt: boolean;
 
     constructor(
+        private formBuilder: FormBuilder,
         private router: Router,
         private loginService: LoginService) {}
 
     ngOnInit() {
         this.auth = new Auth();
         this.message = '';
+        this.configureForm();
     }
 
-    closeAlert(index) {
-        console.log(index);
+    configureForm() {
+        this.form = this.formBuilder.group({
+            email: [null, [Validators.required, Validators.email]],
+            password: [null, Validators.required],
+          });
     }
 
-    onLoggedin() {
+    clearMessage() {
+        this.message = '';
+    }
+
+    isFieldValid(field: string) {
+        return (
+          (!this.form.get(field).valid && this.form.get(field).touched) ||
+          (this.form.get(field).untouched && this.formSumitAttempt)
+        );
+    }
+
+    onLoggedin () {
         this.loginService.authenticated(this.auth).
             subscribe(
                 (data: any) =>  {
--- pdv-front/src/app/login/login.module.ts
+++ pdv-front/src/app/login/login.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { FormBuilder } from '@angular/forms/src/form_builder';
 
 @NgModule({
     imports: [
         CommonModule,
         LoginRoutingModule,
-        FormsModule],
+        FormsModule,
+        ReactiveFormsModule],
     declarations: [LoginComponent],
 })
 export class LoginModule {}
